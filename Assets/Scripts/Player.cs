b483e48d3d2530451496b0c03a7f4601130d9d40
jedrek <jedrekszor@wp.pl>
2020-01-20 13:04:20 +0100

    first

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rgbd;
+    private bool canJump = true;
+
+    [SerializeField] private float height;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rgbd = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void jump()
+    {
+        if (canJump)
+        {
+            rgbd.AddForce(new Vector2(0, height), ForceMode2D.Impulse);
+            canJump = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Floor"))
+        {
+            canJump = true;
+        }
+    }
+}
