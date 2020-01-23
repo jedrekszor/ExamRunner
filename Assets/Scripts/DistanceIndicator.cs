538d7a53e86759613a5ec1b8229e2fad02639fdd
jedrek <jedrekszor@wp.pl>
2020-01-23 19:37:23 +0100

    hfusio

---
+++ Assets/Scripts/DistanceIndicator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceIndicator : MonoBehaviour
+{
+    private GameObject session, player;
+    private float dist;
+    
+    void Start()
+    {
+        session = GameObject.Find("Session");
+        player = GameObject.Find("Player");
+    }
+    
+    void Update()
+    {
+        dist = Mathf.Abs(session.transform.position.x - player.transform.position.x) - 2.8f;
+        transform.localScale = new Vector3(dist/20, transform.localScale.y, transform.localScale.z);
+    }
+}
---
+++ Assets/Scripts/GoTo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoTo : MonoBehaviour
+{
+    public Transform goTo;
+    
+    void Update()
+    {
+        transform.position = new Vector3(goTo.position.x, transform.position.y, transform.position.z);
+    }
+}
